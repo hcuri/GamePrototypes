5ea8a588cae201b3398066ed5b9448606d51f412
Potion Chain <pohsun@usc.edu>
2018-01-30 16:44:26 -0800

    Added a script for create rigidbody automatically
    Now the weapon shall added a rigidbody if self

--- Assets/Script/Po_Script/ThrowableBehave.cs
+++ Assets/Script/Po_Script/ThrowableBehave.cs
@@ -12,12 +12,20 @@ public class ThrowableBehave : MonoBehaviour {
     public int weaponNum;
     public float m_Damage;
     public string m_Type;
+    Rigidbody m_Rigidbody;
 	// Use this for initialization
 	void Start () {
         tag = "Weapon";
         m_Collider = GetComponent<Collider>();
         //make sure the setting is correct
         m_Collider.isTrigger = false;
+        if(GetComponent<Rigidbody>() == null)
+        {
+            this.gameObject.AddComponent<Rigidbody>();
+            m_Rigidbody = GetComponent<Rigidbody>();
+            m_Rigidbody.useGravity = true;
+            m_Rigidbody.isKinematic = true;
+        }
 	}
 	
 	// Update is called once per frame
