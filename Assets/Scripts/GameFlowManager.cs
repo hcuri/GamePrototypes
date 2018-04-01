c973bbe28dc93e6dc288f9053dd57a967b493004
octopopo <godpopo34@gmail.com>
2018-04-01 10:29:49 -0700

    Now it will win and lost for the correct player
    Now it will win and lose for the correct player
    
    But now the remaining player hasn't update correctly

--- Assets/Scripts/GameFlowManager.cs
+++ Assets/Scripts/GameFlowManager.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class GameFlowManager : MonoBehaviour {
 
     public GameObject[] playerInGame;
     public int playerCount;
     public bool playerInGameIsFilled;
-
+    public GameObject pNManager;
     public GameObject endPanel;
     public GameObject winPanel;
+    public bool isDebugging;
     // Use this for initialization
     void Start () {
         playerInGameIsFilled = false;
@@ -19,16 +21,13 @@ public class GameFlowManager : MonoBehaviour {
         winPanel = GameObject.Find("WinPanel");
         endPanel.SetActive(false);
         winPanel.SetActive(false);
+        pNManager = GameObject.Find("NetworkManager");
+        isDebugging = pNManager.GetComponent<PhotonNetworkManager>().debugMode;
+        playerCount = pNManager.GetComponent<PhotonNetworkManager>().numPeopleToStart;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (!playerInGameIsFilled)
-        {
-            playerInGame = GameObject.FindGameObjectsWithTag("Player");
-            playerCount = playerInGame.Length;
-            playerInGameIsFilled = true;
-        }
 	}
 
     public void playerDead(GameObject deadPlayer)
@@ -39,7 +38,7 @@ public class GameFlowManager : MonoBehaviour {
         }
 
         playerCount--;
-        if(playerCount == 1)
+        if(playerCount == 1 && !isDebugging)
         {
             checkWin();
         }
@@ -63,15 +62,23 @@ public class GameFlowManager : MonoBehaviour {
 
     public void checkWin()
     {
+        Debug.Log("Checking Winner");
+        playerInGame = GameObject.FindGameObjectsWithTag("Player");
+        Debug.Log("playerInGame: " + playerInGame);
         foreach(GameObject livePlayer in playerInGame)
         {
-            if(livePlayer.GetComponent<PlayerNetwork>().returnHealth() >= 0.0f)
+            if(livePlayer.GetComponent<PlayerNetwork>().returnHealth() > 0)
             {
+                int id = livePlayer.GetComponent<PhotonView>().ownerId;
 
+                this.GetComponent<PhotonView>().RPC("sendWin", PhotonPlayer.Find(id));
             }
         }
     }
 
     [PunRPC]
-    public void
+    public void sendWin()
+    {
+        winPanel.SetActive(true);
+    }
 }
