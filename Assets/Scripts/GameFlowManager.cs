ad2634882ca1ef590786def10ad7ee8acf8c300c
octopopo <godpopo34@gmail.com>
2018-04-01 10:41:08 -0700

    Player Remaining can be update
    The text of player remaining can be updated perfectly

--- Assets/Scripts/GameFlowManager.cs
+++ Assets/Scripts/GameFlowManager.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.UI;
 
 public class GameFlowManager : MonoBehaviour {
 
     public GameObject[] playerInGame;
     public int playerCount;
+    public int total_Player;
     public bool playerInGameIsFilled;
     public GameObject pNManager;
     public GameObject endPanel;
     public GameObject winPanel;
     public bool isDebugging;
+    public Text playersRemain;
     // Use this for initialization
     void Start () {
         playerInGameIsFilled = false;
@@ -24,11 +27,21 @@ public class GameFlowManager : MonoBehaviour {
         pNManager = GameObject.Find("NetworkManager");
         isDebugging = pNManager.GetComponent<PhotonNetworkManager>().debugMode;
         playerCount = pNManager.GetComponent<PhotonNetworkManager>().numPeopleToStart;
+        total_Player = playerCount;
+        playersRemain = GameObject.Find("PlayersRemain").GetComponent<Text>();
+        playersRemain.text = "";
+        updatePlayerRemain();
     }
 	
 	// Update is called once per frame
 	void Update () {
+        
 	}
+
+    public void updatePlayerRemain()
+    {
+        playersRemain.text = "Player Remaining: " + playerCount + "/" + total_Player;
+    }
 
     public void playerDead(GameObject deadPlayer)
     {
@@ -38,7 +51,9 @@ public class GameFlowManager : MonoBehaviour {
         }
 
         playerCount--;
-        if(playerCount == 1 && !isDebugging)
+
+        updatePlayerRemain();
+        if (playerCount == 1 && !isDebugging)
         {
             checkWin();
         }
@@ -62,9 +77,7 @@ public class GameFlowManager : MonoBehaviour {
 
     public void checkWin()
     {
-        Debug.Log("Checking Winner");
         playerInGame = GameObject.FindGameObjectsWithTag("Player");
-        Debug.Log("playerInGame: " + playerInGame);
         foreach(GameObject livePlayer in playerInGame)
         {
             if(livePlayer.GetComponent<PlayerNetwork>().returnHealth() > 0)
