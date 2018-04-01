69b5b389b315485a45fd4f71ddd14d53435bc17f
octopopo <godpopo34@gmail.com>
2018-04-01 02:29:45 -0700

    Add a GameFlowManager
    Add a simple GameFlowManager to keep track of every players in the game

---
+++ Assets/Scripts/GameFlowManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFlowManager : MonoBehaviour {
+
+    public GameObject[] playerInGame;
+    public int playerCount;
+    public bool playerInGameIsFilled;
+	// Use this for initialization
+	void Start () {
+        playerInGameIsFilled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!playerInGameIsFilled)
+        {
+            playerInGame = GameObject.FindGameObjectsWithTag("Player");
+            playerCount = playerInGame.Length;
+            playerInGameIsFilled = false;
+        }
+	}
+}
